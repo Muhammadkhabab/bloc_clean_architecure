d20c5301551193ead5bcf4720162a90de8e37f59
Query Byte Tech <khababkhattak332@gmail.com>
2025-02-10 23:44:35 +0500

    user model make using freezed package

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,9 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-
 part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
-//todo other file to nto genrated it freezed and model.g.dart file
 @freezed
 class UserModel with _$UserModel {
   const factory UserModel({
