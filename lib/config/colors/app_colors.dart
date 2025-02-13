7da88a467b656774e2e65c98d088a8eada65775c
Query Byte Tech <khababkhattak332@gmail.com>
2025-02-13 23:16:40 +0500

    make the app colors for the app

--- lib/config/colors/app_colors.dart
+++ lib/config/colors/app_colors.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color blackColor = Color(0x0ff00000);
-  static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color buttonColor = Colors.green;
+  // Define all colors as constants
+  static const Color primaryColor = Color(0xFF059669);
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+  static const Color secondaryColor = Color(0xFFF4F4F4);
+  static const Color onSecondaryColor = Color(0xFF000000);
+  static const Color errorColor = Color(0xFFD32F2F);
+  static const Color onErrorColor = Color(0xFFFFFFFF);
+  static const Color surfaceColor = Color(0xFFFFFFFF);
+  static const Color onSurfaceColor = Color(0xFF000000);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color onBackgroundColor = Color(0xFF000000);
+  static const Color borderColor = Color(0xffDADCE0);
+  static const Color bodyTextColor = Color(0xFF7C7C7C);
+  static const Color labelTextColor = Color(0xff8B8B8B);
+  static const Color disabledColor = Color(0xFFE0E0E0);
+  static const Color shadowColor = Color(0x1F000000);
+  static const Color dividerColor = Color(0xFFDADADA);
+  static const Color cardColor = Color(0xFFFFFFFF);
+  static const Color blackCustom = Color(0xFF000000);
+  static const Color iconColor = Color(0xFF838383);
+  static const Color textColour = Color(0xFF333333);
+  static const Color labelColour = Color(0xff999999);
+  static const Color mainText = Color(0xff3D3D3D);
 }
